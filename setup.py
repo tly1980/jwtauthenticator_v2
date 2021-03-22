ae3b9af108daec9c6de2806dd4e2e8709b219d96
pasha <pasha@izihawa.net>
2021-03-22 10:22:50 +0300

    release: 2.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jupyterhub-jwtauthenticator-v2',
-    version='2.0.2',
+    version='2.0.3',
     description='JSONWebToken Authenticator for JupyterHub',
     url='https://github.com/izihawa/jwtauthenticator_v2',
     author='ppodolsky',
